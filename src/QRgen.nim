ed6f64d249f0177a7cc2e12425565ae858d41484
aruZeta <aru_hackZ.official@zohomail.eu>
2022-07-22 15:08:49 +0200

    [add] Proc to calculate the most efficient QR mode

--- src/QRgen.nim
+++ src/QRgen.nim
@@ -17,3 +17,18 @@ type
     errorCorrectionLevel*: QRErrorCorrectionLevel
     data*: string
     sideSize*: uint8 # The QR code is a square, all sides have the same size
+
+const
+  numericValues = {'0'..'9'}
+  alphaNumericValues = {
+    ' ', '$', '%', '*', '+', '-', '.', '/', ':', '0'..'9', 'A'..'Z'
+  }
+
+proc setMostEfficientMode*(qr: QRCode) =
+  qr.mode = qrNumericMode
+  for c in qr.data:
+    if c notin alphaNumericValues:
+      qr.mode = qrByteMode
+      return
+    elif c notin numericValues:
+      qr.mode = qrAlphanumericMode
