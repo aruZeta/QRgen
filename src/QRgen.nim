de6718380d4acde27162125397a57e8dcce167dc
aruZeta <aru_hackZ.official@zohomail.eu>
2022-07-23 11:38:14 +0200

    [refactor] Better naming of QR ECC levels

--- src/QRgen.nim
+++ src/QRgen.nim
@@ -6,11 +6,11 @@ type
     # qrECIMode   = 0b0111 (Extended Channel Interpretation) not supported
     # qrKanjiMode = 0b1000                                   not supported
 
-  QRErrorCorrectionLevel {.pure.} = enum
-    l, #  7% data recovery
-    m, # 15% data recovery
-    q, # 25% data recovery
-    h  # 30% data recovery
+  QRErrorCorrectionLevel = enum
+    qrEccL, #  7% data recovery
+    qrEccM, # 15% data recovery
+    qrEccQ, # 25% data recovery
+    qrEccH  # 30% data recovery
 
   QRCode* = ref object
     mode*: QRMode
