9d9be1304f310ba0f57e538ec6cafa5185b54952
aruZeta <aru_hackZ.official@zohomail.eu>
2022-07-26 16:38:07 +0200

    [add] add bits to BitArray implementation with uint16|32|64

--- src/QRgen/bitArray.nim
+++ src/QRgen/bitArray.nim
@@ -27,3 +27,46 @@ proc add*[T: uint8](b: var BitArray, val: T, len: uint8) =
     b.data[arrPos] += val shl (bitsLeft - len)
 
   b.pos += len
+
+proc add*[T: uint16 | uint32 | uint64](b: var BitArray, val: T, len: uint8) =
+  let tSize: uint8 = 8 * sizeof(T)
+  if len > tSize:
+    raise newException(
+      RangeDefect,
+      "len can't be bigger than " & $tSize & " in a " & $T
+    )
+
+  template checkLen(size: uint8, uintType: typedesc) =
+    if tSize > size and len <= size:
+      raise newException(
+        RangeDefect,
+        "You should use a " & $uintType &
+        " if len is less than or equal to " & $size
+      )
+
+  checkLen 8, uint8
+  checkLen 16, uint16
+  checkLen 32, uint32
+
+  let
+    arrPos: uint32       = b.pos div 8
+    bytePos: uint8       = cast[uint8](b.pos mod 8)
+    bitsLeft: uint8      = 8 - bytePos
+    bytes: uint8         = (len - bitsLeft) div 8
+    remainingBits: uint8 = (len - bitsLeft) mod 8
+
+  if arrPos == cast[uint32](b.data.len): b.data.add 0'u8
+
+  for _ in 1'u8..bytes: b.data.add 0'u8
+
+  if remainingBits > 0:
+    b.data.add cast[uint8](val shl (8 - remainingBits))
+
+  var val = val shr remainingBits
+
+  for i in 0'u8..<bytes:
+    b.data[arrPos + bytes - i] = cast[uint8](val)
+    val = val shr 8
+
+  b.data[arrPos] += cast[uint8](val)
+  b.pos += len
