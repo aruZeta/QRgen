c55e535d3a9825f760a3c0ae387f626a0f8fc60b
aruZeta <aru_hackZ.official@zohomail.eu>
2022-07-26 16:37:27 +0200

    [add] add bits to BitArray

--- src/QRgen/bitArray.nim
+++ src/QRgen/bitArray.nim
@@ -5,3 +5,25 @@ type
 
 proc newBitArray*(): BitArray =
   BitArray(pos: 0, data: @[])
+
+proc add*[T: uint8](b: var BitArray, val: T, len: uint8) =
+  let tSize: uint8 = 8 * sizeof(T)
+  if len > tSize:
+    raise newException(
+      RangeDefect,
+      "len can't be bigger than " & $tSize & " in a " & $T
+    )
+
+  let
+    arrPos: uint32 = b.pos div 8
+    bytePos: uint8 = cast[uint8](b.pos mod 8)
+    bitsLeft: uint8 = 8 - bytePos
+
+  if bitsLeft < len:
+    b.data.add(val shl bitsLeft)
+    b.data[arrPos] += val shr bytePos
+  else:
+    if arrPos == cast[uint32](b.data.len): b.data.add 0'u8
+    b.data[arrPos] += val shl (bitsLeft - len)
+
+  b.pos += len
