6eb64ccc86be38c6c99d657d2c3e848a624a3044
aruZeta <aru_hackZ.official@zohomail.eu>
2022-08-15 23:28:13 +0200

    [add] Export draw proc in main module

--- src/QRgen.nim
+++ src/QRgen.nim
@@ -1,5 +1,6 @@
-import QRgen/private/[QRCode, EncodedQRCode, qrTypes]
+import QRgen/private/[QRCode, EncodedQRCode, DrawedQRCode, qrTypes]
 
 export newQRCode, setMostEfficientMode, setSmallestVersion
 export encode
+export draw
 export qrTypes
