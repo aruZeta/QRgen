48d22d097017d373f9aeafa1787b1204ec3d6e9b
aruZeta <aru_hackZ.official@zohomail.eu>
2022-08-16 12:54:22 +0200

    [add] drawAlignmentPatterns proc and tests
    Note that, for the moment, the tests need to be reviewed visually to
    see if the QR code was drawed well or not, since comparing the
    resulting matrix with the correct one means to copy and paste a huge
    chunck of 0's and 1's lol (like the tests with encoding).

--- src/QRgen/private/DrawedQRCode.nim
+++ src/QRgen/private/DrawedQRCode.nim
@@ -1,4 +1,4 @@
-import Drawing, EncodedQRCode, qrTypes
+import Drawing, EncodedQRCode, qrTypes, qrCapacities
 
 type
   DrawedQRCode* = object
@@ -16,6 +16,32 @@ proc drawFinderPatterns*(d: var DrawedQRCode) =
   drawFinderPattern d.drawing.size - 8'u8, 0'u8
   drawFinderPattern 0'u8, d.drawing.size - 8'u8
 
+proc calcVersion(size: uint8): QRVersion =
+  (size - 21'u8) div 4'u8 + 1'u8
+
+proc drawAlignmentPatterns*(d: var DrawedQRCode) =
+  const version2Size: uint8 = 1 * 4 + 21
+  if d.drawing.size < version2Size:
+    return
+
+  template drawFinderPattern(x, y: uint8) =
+    d.drawing.fillPoint     x,        y
+    d.drawing.fillRectangle x-2..x+2, y-2
+    d.drawing.fillRectangle x-2..x+2, y+2
+    d.drawing.fillRectangle x-2,      y-1..y+1
+    d.drawing.fillRectangle x+2,      y-1..y+1
+
+  let locations = alignmentPatternLocations[calcVersion d.drawing.size]
+  for i, pos in locations:
+    if i < locations.len - 1:
+      drawFinderPattern 6'u8, pos
+      drawFinderPattern pos, 6'u8
+
+    for pos2 in locations[i..<locations.len]:
+      drawFinderPattern pos, pos2
+      if pos != pos2:
+        drawFinderPattern pos2, pos
+
 proc newDrawedQRCode*(version: QRVersion): DrawedQRCode =
   DrawedQRCode(drawing: newDrawing((version - 1) * 4 + 21))
 
@@ -26,3 +52,4 @@ proc draw*(qr: EncodedQRCode): DrawedQRCode =
   result = newDrawedQRCode qr.version
 
   result.drawFinderPatterns
+  result.drawAlignmentPatterns
--- tests/testDrawedQRCode.nim
+++ tests/testDrawedQRCode.nim
@@ -6,3 +6,19 @@ test "Finder patterns":
   qr.drawFinderPatterns
 
   qr.drawing.print dpTerminal
+
+test "Alignment patterns":
+  var qr1 = newQRCode("Hello World", version = 2).encode.newDrawedQRCode
+  qr1.drawFinderPatterns
+  qr1.drawAlignmentPatterns
+  qr1.drawing.print dpTerminal
+
+  var qr2 = newQRCode("Hello World", version = 7).encode.newDrawedQRCode
+  qr2.drawFinderPatterns
+  qr2.drawAlignmentPatterns
+  qr2.drawing.print dpTerminal
+
+  var qr3 = newQRCode("Hello World", version = 14).encode.newDrawedQRCode
+  qr3.drawFinderPatterns
+  qr3.drawAlignmentPatterns
+  qr3.drawing.print dpTerminal
