958f8b39ce1a8d6fda904aa1d6f7b9e11194513b
aruZeta <aru_hackZ.official@zohomail.eu>
2022-08-20 23:01:30 +0200

    [refactor] Better BitArray add proc code

--- src/QRgen/private/BitArray.nim
+++ src/QRgen/private/BitArray.nim
@@ -16,7 +16,8 @@ proc nextByte*(b: var BitArray, evenIfBlank: bool = false): uint8 =
 
 proc add*[T: uint8 | uint16 | uint32 | uint64
          ](b: var BitArray, val: T, len: uint8) =
-  let tSize: uint8 = 8 * sizeof(T)
+  template tSize: int = 8 * sizeof(T)
+
   if len > tSize:
     raise newException(
       RangeDefect,
@@ -25,29 +26,33 @@ proc add*[T: uint8 | uint16 | uint32 | uint64
   elif len == 0:
     return
 
+  template castU8(expr: untyped): uint8 =
+    ## If T is not u8, cast val to u8 when putting it in the BitArray
+    when T isnot uint8: cast[uint8](expr)
+    else:               expr
+
   let
     arrPos:   uint16 = b.pos div 8
-    bytePos:  uint8  = cast[uint8](b.pos mod 8)
-    bitsLeft: uint8  = 8 - bytePos
-    bytes:    uint8  = if len <= bitsLeft: 0'u8
-                       else: (len - bitsLeft) div 8
-    remBits:  uint8  = if len <= bitsLeft: 0'u8
-                       else: (len - bitsLeft) mod 8
-
-  if remBits > 0:
-    b.data[arrPos + bytes + 1] = cast[uint8](val shl (8 - remBits))
-
-  var val = val shr remBits
-
-  for i in 0'u8..<bytes:
-    b.data[arrPos + bytes - i] = cast[uint8](val)
-    val = val shr 8
+    bitsLeft: uint8  = 8 - cast[uint8](b.pos mod 8)
 
   if len <= bitsLeft:
-    let mask: uint8 = 0b11111111'u8 shr (8 - len)
-    b.data[arrPos] += cast[uint8]((val and mask) shl (bitsLeft - len))
+    b.data[arrPos] += castU8(
+      (val and (0xFF'u8 shr (8 - len))) shl (bitsLeft - len)
+    )
   else:
-    let mask: uint8 = 0b11111111'u8 shr (8 - bitsLeft)
-    b.data[arrPos] += cast[uint8](val and mask)
+    let
+      bytes:   uint8  = (len - bitsLeft) div 8
+      remBits: uint8  = (len - bitsLeft) mod 8
+
+    if remBits > 0:
+      b.data[arrPos + bytes + 1] = castU8(val shl (8 - remBits))
+
+    var val = val shr remBits
+
+    for i in 0'u8..<bytes:
+      b.data[arrPos + bytes - i] = castU8(val)
+      val = val shr 8
+
+    b.data[arrPos] += castU8(val and (0xFF'u8 shr (8 - bitsLeft)))
 
   b.pos += len
