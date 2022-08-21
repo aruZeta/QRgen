a67f5fcd56d799de8868ac14e4673552682bb9d5
aruZeta <aru_hackZ.official@zohomail.eu>
2022-08-21 11:16:01 +0200

    [refactor] Better Drawing code and remove unnecessary stuff

--- src/QRgen/private/Drawing.nim
+++ src/QRgen/private/Drawing.nim
@@ -11,23 +11,25 @@ proc newDrawing*(size: uint8): Drawing =
   result = Drawing(matrix: newSeqOfCap[uint8](matrixSize), size: size)
   result.matrix.setLen(matrixSize)
 
-proc `[]=`(d: var Drawing, y, x: uint8, val: bool) =
+proc `[]=`*(d: var Drawing, x, y: uint8, val: bool) =
   let
     bitPos:  uint16 = cast[uint16](y) * d.size + x
     arrPos:  uint16 = bitPos div 8
     bytePos: uint8  = cast[uint8](bitPos mod 8)
 
   if val:
-    d.matrix[arrPos] = d.matrix[arrPos] or
-                       (0x01'u8 shl (7 - bytePos))
+    d.matrix[arrPos] = d.matrix[arrPos] or (0x01'u8 shl (7 - bytePos))
   else:
-    d.matrix[arrPos] = d.matrix[arrPos] and
-                       (0xFF'u8 xor (0x01'u8 shl (7 - bytePos)))
+    d.matrix[arrPos] =
+      d.matrix[arrPos] and (0xFF'u8 xor (0x01'u8 shl (7 - bytePos)))
+
+template fillPoint*(d: var Drawing, x, y: uint8) =
+  d[x, y] = true
 
 proc fillRectangle*(d: var Drawing, width, height: Slice[uint8]) =
   for y in height:
     for x in width:
-      d[y, x] = true
+      d[x, y] = true
 
 template fillRectangle*(d: var Drawing, width: Slice[uint8], height: uint8) =
   fillRectangle(d, width, height..height)
@@ -38,23 +40,12 @@ template fillRectangle*(d: var Drawing, width: uint8, height: Slice[uint8]) =
 template fillRectangle*(d: var Drawing, size: Slice[uint8]) =
   fillRectangle(d, size, size)
 
-template fillPoint*(d: var Drawing, width, height: uint8) =
-  fillRectangle(d, width..width, height..height)
-
-template fillRow*(d: var Drawing, row: uint8) =
-  fillRectangle(d, 0'u8..d.size-1, row..row)
-
-template fillColumn*(d: var Drawing, column: uint8) =
-  fillRectangle(d, column..column, 0'u8..d.size-1)
-
 proc printTerminal*(d: Drawing) =
   for y in 0'u8..<d.size:
     for x in 0'u8..<d.size:
-      let
-        bitPos:  uint16 = cast[uint16](y) * d.size + x
-        arrPos:  uint16 = bitPos div 8
-        bytePos: uint8  = cast[uint8](bitPos mod 8)
-      if ((d.matrix[arrPos] shr (7 - bytePos)) and 1'u8) == 1'u8:
+      let bitPos:  uint16 = cast[uint16](y) * d.size + x
+      if ((d.matrix[bitPos div 8] shr
+          (7 - (cast[uint8](bitPos mod 8)))) and 1'u8) == 1'u8:
         stdout.write "██"
       else:
         stdout.write "  "
