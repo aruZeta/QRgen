aruZeta/QRgen

commit 5212e425701248850cdf655c5dced466e8e7aa7a

[refactor] Making BitArray fields private
This makes the BitArray implementation encapsulated, and all
operations over it are made via procedures.

Issue: aruZeta/QRgen#4

src/QRgen/private/BitArray.nim
@@ -17,26 +17,14 @@ type
     ## A BitArray object used by `EncodedQRCode<EncodedQRCode.html>`_.
     ##
     ## `pos` is used to keep track of where new bits will be placed.
-    pos*: uint16
-    data*: seq[uint8]
+    pos: uint16
+    data: seq[uint8]
 
 proc newBitArray*(size: uint16): BitArray =
   ## Creates a new `BitArray` object whose `data` will have a cap of `size`
   ## and it's len will also be set to `size`.
   result = BitArray(pos: 0, data: newSeqOfCap[uint8](size))
   result.data.setLen(size)
-
-template `[]`*(self: BitArray, i: SomeInteger): uint8 =
-  ## A shorthand to writing `myBitArray.data[i]`
-  self.data[i]
-
-template `[]`*(self: BitArray, i: Slice[SomeInteger]): seq[uint8] =
-  ## A shorthand to writing `myBitArray.data[i1..i2]`
-  self.data[i]
-
-template `[]=`*(self: BitArray, i: SomeInteger, val: uint8) =
-  ## A shorthand to writing `myBitArray.data[i] = val`
-  self.data[i] = val
 
 proc nextByte*(self: var BitArray): uint8 =
   ## Moves `pos` to the next byte, unless it is pointing to the start of an
@@ -75,7 +63,7 @@ proc add*(self: var BitArray, val: SomeUnsignedInt, len: uint8) =
     arrPos: uint16 = self.pos div 8
     bitsLeft: uint8 = 8 - cast[uint8](self.pos mod 8)
   if len <= bitsLeft:
-    self[arrPos] += castU8(
+    self.data[arrPos] += castU8(
       (val and (0xFF'u8 shr (8 - len))) shl (bitsLeft - len)
     )
   else:
@@ -83,10 +71,35 @@ proc add*(self: var BitArray, val: SomeUnsignedInt, len: uint8) =
       bytes: uint8 = (len - bitsLeft) div 8
       remBits: uint8 = (len - bitsLeft) mod 8
     if remBits > 0:
-      self[arrPos + bytes + 1] = castU8(val shl (8 - remBits))
+      self.data[arrPos + bytes + 1] = castU8(val shl (8 - remBits))
     var val = val shr remBits
     for i in 0'u8..<bytes:
-      self[arrPos + bytes - i] = castU8(val)
+      self.data[arrPos + bytes - i] = castU8(val)
       val = val shr 8
-    self[arrPos] += castU8(val and (0xFF'u8 shr (8 - bitsLeft)))
+    self.data[arrPos] += castU8(val and (0xFF'u8 shr (8 - bitsLeft)))
   self.pos += len
+
+proc unsafeAdd*(self: var BitArray, val: uint8) =
+  self.data.add val
+
+proc unsafeDelete*(self: var BitArray, pos: uint16) =
+  self.data.delete pos
+
+# Getters/setters:
+
+proc `[]`*(self: BitArray, i: SomeInteger): uint8 =
+  self.data[i]
+
+proc `[]`*[T,S](self: BitArray, i: HSlice[T,S]): seq[uint8] =
+  self.data[i]
+
+proc `[]=`*(self: var BitArray, i: SomeInteger, val: uint8) =
+  self.data[i] = val
+
+proc pos*(self: BitArray): uint16 = self.pos
+proc data*(self: BitArray): seq[uint8] = self.data
+proc len*(self: BitArray): int = self.data.len
+
+# - Used only in testing:
+
+proc `data=`*(self: var BitArray, val: seq[uint8]) = self.data = val

src/QRgen/private/DrawedQRCode.nim
@@ -113,8 +113,8 @@ proc drawData*(self: var DrawedQRCode, data: BitArray) =
       pos.repeat -= 1
     else:
       elseBody
-  for module in 0..<data.data.len * 8:
-    if ((data.data[module div 8] shr (7 - (module mod 8))) and 0x01) == 0x01:
+  for module in 0..<data.len*8:
+    if ((data[module div 8] shr (7 - (module mod 8))) and 0x01) == 0x01:
       self.fillPoint pos.x, pos.y
     # Avoiding stuff, changing orientation in borders, etc block
     case pos.direction
@@ -440,7 +440,7 @@ proc draw*(qr: EncodedQRCode): DrawedQRCode =
   result.drawAlignmentPatterns
   result.drawTimingPatterns
   result.drawDarkModule
-  result.drawData qr.encodedData
+  result.drawData qr.data
   result.applyBestMaskPattern
   result.drawFormatInformation
   result.drawVersionInformation
@@ -451,4 +451,4 @@ proc drawOnly*(qr: EncodedQRCode): DrawedQRCode =
   result.drawAlignmentPatterns
   result.drawTimingPatterns
   result.drawDarkModule
-  result.drawData qr.encodedData
+  result.drawData qr.data

src/QRgen/private/EncodedQRCode.nim
@@ -6,28 +6,16 @@ type
     mode*: QRMode
     version*: QRVersion
     ecLevel*: QRECLevel
-    encodedData*: BitArray
+    data*: BitArray
 
 template `[]`[T](self: QRCapacity[T], qr: EncodedQRCode): T =
   self[qr.ecLevel][qr.version]
 
-template `[]`(self: EncodedQRCode, i: SomeInteger): uint8 =
-  self.encodedData[i]
-
-template `[]=`(self: EncodedQRCode, i: SomeInteger, val: uint8) =
-  self.encodedData[i] = val
-
-template add(self: EncodedQRCode, val: SomeUnsignedInt, len: uint8) =
-  self.encodedData.add val, len
-
-template unsafeAdd(self: EncodedQRCode, val: uint8) =
-  self.encodedData.data.add val
-
-template unsafeDelete(self: EncodedQRCode, pos: uint16) =
-  self.encodedData.data.delete pos
+template `^=`(self: untyped, expr: untyped) =
+  self = self xor expr
 
 proc encodeModeIndicator(self: var EncodedQRCode) =
-  self.encodedData.add cast[uint8](self.mode), 4
+  self.data.add cast[uint8](self.mode), 4
 
 proc encodeCharCountIndicator*(self: var EncodedQRCode, data: string) =
   template modeCases(numericVal, alphanumericVal, byteVal: uint8): uint8 =
@@ -35,7 +23,7 @@ proc encodeCharCountIndicator*(self: var EncodedQRCode, data: string) =
     of qrNumericMode: numericVal
     of qrAlphanumericMode: alphanumericVal
     of qrByteMode: byteVal
-  self.add(cast[uint16](data.len), (
+  self.data.add(cast[uint16](data.len), (
     if self.version <= 9:    modeCases 10, 9, 8
     elif self.version >= 27: modeCases 14, 13, 16
     else:                    modeCases 12, 11, 16
@@ -52,7 +40,7 @@ template getVal8(c: char): uint8 = getAlphanumericValue c
 
 template encodeNumericModeData(self: var EncodedQRCode, data: string) =
   for i in step(0, data.len, 3):
-    self.add(
+    self.data.add(
       getVal16(data[i]) * 100 + getVal16(data[i+1]) * 10 + getVal8(data[i+2]),
       (if data[i] == '0':
          if data[i+1] == '0': 4'u8
@@ -60,19 +48,19 @@ template encodeNumericModeData(self: var EncodedQRCode, data: string) =
        else: 10'u8)
     )
   case (data.len mod 3)
-  of 1: self.add getVal8(data[^1]), 4
-  of 2: self.add getVal16(data[^2]) * 10 + getVal8(data[^1]), 7
+  of 1: self.data.add getVal8(data[^1]), 4
+  of 2: self.data.add getVal16(data[^2]) * 10 + getVal8(data[^1]), 7
   else: discard
 
 template encodeAlphanumericModeData(self: var EncodedQRCode, data: string) =
   for i in step(0, data.len, 2):
-    self.add getVal16(data[i]) * 45 + getVal8(data[i+1]), 11
+    self.data.add getVal16(data[i]) * 45 + getVal8(data[i+1]), 11
   if (data.len mod 2) == 1:
-    self.add getVal8(data[^1]), 6
+    self.data.add getVal8(data[^1]), 6
 
 template encodeByteModeData(self: var EncodedQRCode, data: string) =
   for c in convert(data, "ISO 8859-1", "UTF-8"):
-    self.add cast[uint8](c), 8
+    self.data.add cast[uint8](c), 8
 
 proc encodeData(self: var EncodedQRCode, data: string) =
   case self.mode
@@ -82,24 +70,24 @@ proc encodeData(self: var EncodedQRCode, data: string) =
 
 proc finishEncoding(self: var EncodedQRCode) =
   var missingBits: uint16 =
-    (totalDataCodewords[self] * 8) - self.encodedData.pos
+    (totalDataCodewords[self] * 8) - self.data.pos
   let terminatorBits: uint8 =
     if missingBits > 4: 4'u8
     else: cast[uint8](missingBits)
-  self.add 0b0000'u8, terminatorBits
-  missingBits -= terminatorBits + self.encodedData.nextByte
+  self.data.add 0b0000'u8, terminatorBits
+  missingBits -= terminatorBits + self.data.nextByte
   let missingBytes: uint16 = missingBits div 8
   for _ in 1'u16..(missingBytes div 2):
-    self.add 0b11101100'u8, 8
-    self.add 0b00010001'u8, 8
+    self.data.add 0b11101100'u8, 8
+    self.data.add 0b00010001'u8, 8
   if (missingBytes mod 2) == 1:
-    self.add 0b11101100'u8, 8
+    self.data.add 0b11101100'u8, 8
 
 proc gf256Mod285Multiply(x, y: uint8): uint8 =
   result = 0
   for i in 0'u8..7'u8:
     result = cast[uint8]((result shl 1) xor ((result shr 7) * 0x11D))
-    result = result xor (((y shr (7 - i)) and 0x01) * x)
+    result ^= ((y shr (7 - i)) and 0x01) * x
 
 proc calcGeneratorPolynomial(self: EncodedQRCode): seq[uint8] =
   let degree: uint8 = blockECCodewords[self]
@@ -112,7 +100,7 @@ proc calcGeneratorPolynomial(self: EncodedQRCode): seq[uint8] =
   for _ in 0'u8..<degree:
     for i in 0'u8..<degree-1:
       mult(i)
-      result[i] = result[i] xor result[i+1]
+      result[i] ^= result[i+1]
     mult(^1)
     root = gf256Mod285Multiply(root, 0x02)
 
@@ -135,12 +123,11 @@ proc encodeECC(self: var EncodedQRCode) =
     for j in positions[i]..<positions[i+1]:
       let actualEcPos: uint16 =
         positions[^1] + (cast[uint16](blockECCodewords[self]) * i)
-      let factor = self[j] xor self[actualEcPos]
-      self.unsafeDelete actualEcPos
-      self.unsafeAdd 0
+      let factor = self.data[j] xor self.data[actualEcPos]
+      self.data.unsafeDelete actualEcPos
+      self.data.unsafeAdd 0
       for k in 0'u8..<degree:
-        self[actualEcPos+k] =
-          self[actualEcPos+k] xor (gf256Mod285Multiply(generator[k], factor))
+        self.data[actualEcPos+k] ^= (gf256Mod285Multiply(generator[k], factor))
 
 proc interleaveData*(self: var EncodedQRCode) =
   if group1Blocks[self] == 1 and group2Blocks[self] == 0:
@@ -160,10 +147,10 @@ proc interleaveData*(self: var EncodedQRCode) =
         for j in 0'u16..<g2b:
           yield g2bStart + j * g2c + i
   var
-    dataCopy: seq[uint8] = self.encodedData.data
+    dataCopy: seq[uint8] = self.data.data
     i: int16 = 0
   for pos in codewordPositions():
-    self[i] = dataCopy[pos]
+    self.data[i] = dataCopy[pos]
     i.inc
 
 proc calcEcBlockPositions(self: EncodedQRCode): seq[uint16] =
@@ -179,11 +166,11 @@ proc interleaveECC(self: var EncodedQRCode) =
       (cast[uint16](group2Blocks[self]) * group2BlockDataCodewords[self])
     positions: seq[uint16] = self.calcEcBlockPositions
   var
-    dataCopy: seq[uint8] = self.encodedData.data[firstEcBlockPos..^1]
+    dataCopy: seq[uint8] = self.data[firstEcBlockPos..^1]
     n: uint16 = firstEcBlockPos
   for i in 0'u8..<blockECCodewords[self]:
     for j in positions:
-      self[n] = dataCopy[j+i]
+      self.data[n] = dataCopy[j+i]
       n.inc
 
 proc newEncodedQRCode*(version: QRVersion,
@@ -197,7 +184,7 @@ proc newEncodedQRCode*(version: QRVersion,
   EncodedQRCode(mode: mode,
                 version: version,
                 ecLevel: ecLevel,
-                encodedData: newBitArray(dataSize + eccSize))
+                data: newBitArray(dataSize + eccSize))
 
 proc newEncodedQRCode*(qr: QRCode): EncodedQRCode =
   template dataSize: uint16 = totalDataCodewords[qr]
@@ -206,7 +193,7 @@ proc newEncodedQRCode*(qr: QRCode): EncodedQRCode =
   EncodedQRCode(mode: qr.mode,
                 version: qr.version,
                 ecLevel: qr.ecLevel,
-                encodedData: newBitArray(dataSize + eccSize))
+                data: newBitArray(dataSize + eccSize))
 
 proc encode*(qr: QRCode): EncodedQRCode =
   result = newEncodedQRCode qr

tests/testBitArray.nim
@@ -4,16 +4,16 @@ import QRgen/private/BitArray
 benchmarkTest "Simple bit array":
   var b = newBitArray(1)
   b.add 12'u8, 8
-  check b.data[0] == 12
+  check b[0] == 12
 
 benchmarkTest "Complex bit array":
   var b = newBitArray(3)
   b.add 12'u8, 8
   b.add 24'u8, 8
   b.add 0b101'u8, 3
-  check b.data[0] == 12
-  check b.data[1] == 24
-  check b.data[2] == 160
+  check b[0] == 12
+  check b[1] == 24
+  check b[2] == 160
 
 benchmarkTest "Passing different uint types to bit array":
   var b = newBitArray(9)

tests/testDrawedQRCode.nim
@@ -1,5 +1,5 @@
 import myTestSuite
-import QRgen/private/[DrawedQRCode, EncodedQRCode, QRCode, Drawing, qrTypes]
+import QRgen/private/[BitArray, DrawedQRCode, EncodedQRCode, QRCode, Drawing, qrTypes]
 
 benchmarkTest "drawFinderPatterns()":
   var qr1 = newDrawedQRCode 1
@@ -153,8 +153,8 @@ benchmarkTest "drawDarkModule()":
 
 benchmarkTest "drawOnly() filling all data modules":
   template fill(qr: EncodedQRCode) =
-    for i in 0..<qr.encodedData.data.len:
-      qr.encodedData.data[i] = 0xFF
+    for i in 0..<qr.data.len:
+      qr.data[i] = 0xFF
 
   var qr1 = newEncodedQRCode(1, qrECL)
   fill qr1
@@ -193,8 +193,8 @@ benchmarkTest "drawOnly() filling all data modules":
 
 benchmarkTest "applyMaskPattern() with all data modules filled":
   template fill(qr: EncodedQRCode) =
-    for i in 0..<qr.encodedData.data.len:
-      qr.encodedData.data[i] = 0xFF
+    for i in 0..<qr.data.len:
+      qr.data[i] = 0xFF
 
   template createQR(varName: untyped, version: QRVersion) {.dirty.} =
     var `encoded varName` = newEncodedQRCode(version, qrECL)
@@ -206,7 +206,7 @@ benchmarkTest "applyMaskPattern() with all data modules filled":
     for y in 8'u8..<varName.drawing.size-8:
       for x in 0'u8..1'u8:
         varName.drawing[x, y] = false
-    varName.drawData `encoded varName`.encodedData
+    varName.drawData `encoded varName`.data
     varName.applyMaskPattern mask
 
   let masks: array[8, proc] = [mask0, mask1, mask2, mask3, mask4, mask5, mask6, mask7]

commit ed48a9cb3f92d5d5ccf42ff0b8fa140853efcb37

[add, fix] 5212e42 broke the docs and added more stuff
Issue: aruZeta/QRgen#2

src/QRgen/private/BitArray.nim
@@ -2,15 +2,14 @@
 ##
 ## This module implements a simple `BitArray` object which consists of a
 ## regular `seq[uint8]` and a `pos`.
-## It only provides 2 procedures, one to add more bits to the BitArray,
+##
+## The `BitArray` object is encapsulated, hence, in order to access it's
+## attributes there are some procedures that can be used.
+##
+## There are 2 main procedures, one to add more bits to the BitArray,
 ## `add<#add%2CBitArray%2CSomeUnsignedInt%2Cuint8>`_,
 ## and another to skip to the next byte,
 ## `nextByte<#nextByte%2CBitArray>`_.
-##
-## For ease of use there are some templates to help write less code, like
-## `[]<#[].t%2CBitArray%2CSlice[SomeInteger]>`_,
-## so instead of writing `myBitArray.data[i]` you would write `myBitArray[i]`,
-## as if `BitArray` was a regular `seq[uint8]`.
 
 type
   BitArray* = object
@@ -80,26 +79,48 @@ proc add*(self: var BitArray, val: SomeUnsignedInt, len: uint8) =
   self.pos += len
 
 proc unsafeAdd*(self: var BitArray, val: uint8) =
+  ## Add a `val` to the end of `data`.
+  ##
+  ## .. warning:: This is an unsafe procedure since, by default, `data`'s len
+  ##    is set to it's max capacity and `pos` is not updated.
   self.data.add val
 
 proc unsafeDelete*(self: var BitArray, pos: uint16) =
+  ## Remove the value from `data` in position `pos`.
+  ##
+  ## .. warning:: This is an unsafe procedure since `pos` is not updated.
   self.data.delete pos
 
-# Getters/setters:
+# Getters/setters section
 
 proc `[]`*(self: BitArray, i: SomeInteger): uint8 =
+  ## Getter. Get the value of `data` in index `i`.
   self.data[i]
 
 proc `[]`*[T,S](self: BitArray, i: HSlice[T,S]): seq[uint8] =
+  ## Getter. Get the values of `data` in the slice `i`.
   self.data[i]
 
 proc `[]=`*(self: var BitArray, i: SomeInteger, val: uint8) =
+  ## Setter. Set the value of `data` in index `i` to `val`.
   self.data[i] = val
 
-proc pos*(self: BitArray): uint16 = self.pos
-proc data*(self: BitArray): seq[uint8] = self.data
-proc len*(self: BitArray): int = self.data.len
+proc pos*(self: BitArray): uint16 =
+  ## Getter. Get the value of `self.pos`.
+  self.pos
+
+proc data*(self: BitArray): seq[uint8] =
+  ## Getter. Get the value of `self.data`.
+  self.data
+
+proc len*(self: BitArray): int =
+  ## Getter. Get the value of `self.data`.
+  self.data.len
 
 # - Used only in testing:
 
-proc `data=`*(self: var BitArray, val: seq[uint8]) = self.data = val
+proc `data=`*(self: var BitArray, val: seq[uint8]) =
+  ## Setter. Set the value of `self.data` to `val`.
+  ##
+  ## .. note:: At the moment it's only used in tests.
+  self.data = val