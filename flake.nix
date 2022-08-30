cfd743a1b671d5f1cdcef7c9efb4727c99697942
aruZeta <aru_hackZ.official@zohomail.eu>
2022-08-30 18:23:39 +0200

    [add] Moved shell.nix to flake.nix
    Issue: aruZeta/QRgen#7

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Flake with nim 1.6.6 containing dochack and tools";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-22.05";
+    nixpkgs-nim-1-6-6.url = "nixpkgs/4fc665856d5a6be6f647fd9d63d9390f48763192";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-nim-1-6-6 }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      nim-1-6-6 =
+        nixpkgs-nim-1-6-6.legacyPackages.${system}.nim-unwrapped.overrideAttrs
+          (import ./nix/overlays/nim-dochack.nix);
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          nim-1-6-6
+          nixpkgs-nim-1-6-6.legacyPackages.${system}.nim
+        ];
+      };
+    };
+}
---
+++ nix/overlays/nim-dochack.nix
@@ -0,0 +1,20 @@
+old: {
+    buildPhase = ''
+      runHook preBuild
+      local HOME=$TMPDIR
+      ./bin/nim c koch
+      ./koch boot $kochArgs --parallelBuild:$NIX_BUILD_CORES
+      ./koch toolsNoExternal $kochArgs --parallelBuild:$NIX_BUILD_CORES
+      ./bin/nim js $kochArgs tools/dochack/dochack.nim
+      runHook postBuild
+    ''; # added dockhack.js
+
+    installPhase = ''
+      runHook preInstall
+      install -Dt $out/bin bin/*
+      ln -sf $out/nim/bin/nim $out/bin/nim
+      ./install.sh $out
+      runHook postInstall
+      cp -r tools $out/nim
+    ''; # added tools folder
+}
