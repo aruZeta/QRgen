3ef341e4456d33a150ca03e714669d0649f1411b
aruZeta <aru_hackZ.official@zohomail.eu>
2022-08-31 10:40:48 +0200

    [add] Missed to commit proof used in 05033b5
    This is why if there exists G2 blocks, I added another loop to add one
    more codeword of each of them.

---
+++ tests/testQRCapacities.nim
@@ -0,0 +1,14 @@
+import QRgen/private/[qrCapacities, qrTypes]
+import std/unittest
+
+test "G1 codewords < G2 codewords":
+  for l in QRECLevel:
+    for v in QRVersion.low..QRVersion.high:
+      if group2Blocks[l][v] > 0:
+        check group1BlockDataCodewords[l][v] < group2BlockDataCodewords[l][v]
+
+test "G2 codewords - G1 codewords == 1":
+  for l in QRECLevel:
+    for v in QRVersion.low..QRVersion.high:
+      if group2Blocks[l][v] > 0:
+        check group2BlockDataCodewords[l][v] - group1BlockDataCodewords[l][v] == 1
