070532ab362450f6d848c2ed3a5f0f81e1b8724c
aruZeta <aru_hackZ.official@zohomail.eu>
2022-09-01 17:56:11 +0200

    [add] qrCharacters documentation
    Issue: aruZeta/QRgen#2

--- src/QRgen/private/qrCharacters.nim
+++ src/QRgen/private/qrCharacters.nim
@@ -1,3 +1,12 @@
+## # QR Characters
+##
+## This module contains some utilities to deal with encoding and QR mode
+## detection.
+##
+## .. note:: The "alphanumeric table" is mentioned some times in this document,
+##    for reference you can find it in thonky's website,
+##    `here<https://www.thonky.com/qr-code-tutorial/alphanumeric-table>`_.
+
 const
   numericValues*      = {'0'..'9'}
   alphaValues*        = {'A'..'Z'}
@@ -5,17 +14,25 @@ const
   alphaNumericValues* = specialValues + numericValues + alphaValues
 
 iterator pairs[T](a: set[T]): tuple[key: uint8, val: T] =
+  ## Iterator to get both index and value off a `set`.
+  ##
+  ## .. note:: The order in which the values are given is not by order of
+  ##    insertion.
   var x: uint8 = 0'u8
   for c in a:
     yield (key: x, val: c)
     inc x
 
 proc getSpecialValue(c: char): uint8 =
+  ## Returns the value of the special character `c` according to the
+  ## alphanumeric table.
   for i, val in specialValues:
     if val == c:
       return i + 36'u8
 
 proc getAlphanumericValue*(c: char): uint8 =
+  ## Returns the value of the alphanumeric character `c` according to the
+  ## alphanumeric table, else returns `0xFF` (`uint8.high`).
   const
     firstNumericPos: uint8 = cast[uint8]('0')
     firstAlphaPos: uint8 = cast[uint8]('A') - 10
