d1cbb6be7009f52dd9a0f06e15922423703e2dde
aruZeta <aru_hackZ.official@zohomail.eu>
2022-09-01 17:24:56 +0200

    [add] qrTypes documentation

--- src/QRgen/private/qrTypes.nim
+++ src/QRgen/private/qrTypes.nim
@@ -1,5 +1,11 @@
+## # QR Types
+##
+## This module compromises of a collection of commonly used types across almost
+## all modules.
+
 type
   QRMode* {.size: sizeof(uint8).} = enum
+    ## Denotes the mode a QR code is to be encoded with.
     qrNumericMode      = 0b0001,
     qrAlphanumericMode = 0b0010,
     qrByteMode         = 0b0100,
@@ -7,9 +13,12 @@ type
     # qrKanjiMode = 0b1000                                   not supported
 
   QRECLevel* {.size: sizeof(uint8).} = enum
+    ## Denotes the Error Correction level which will be used to create
+    ## the error correction codewords (ECCs) of the QR code.
     qrECL, #  7% data recovery
     qrECM, # 15% data recovery
     qrECQ, # 25% data recovery
     qrECH  # 30% data recovery
 
   QRVersion* = range[1'u8..40'u8]
+    ## Denotes the version of the QR code, which also indicates it's size.
