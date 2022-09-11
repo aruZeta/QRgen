5cae91aabcf0ab0095643a8f503853c837972f54
aruZeta <aru_hackZ.official@zohomail.eu>
2022-09-11 11:05:23 +0200

    [refactor] Removing duplicate code in `print` with templates

--- src/QRgen/private/DrawedQRCode/print.nim
+++ src/QRgen/private/DrawedQRCode/print.nim
@@ -7,6 +7,8 @@ when defined(js):
   import
     std/[jsconsole]
 
+template size: uint8 = self.drawing.size
+
 proc printTerminal*(self: DrawedQRCode) =
   ## Print a `DrawedQRCode` to the terminal using `stdout`.
   template log(s: string) =
@@ -15,9 +17,9 @@ proc printTerminal*(self: DrawedQRCode) =
     else:
       stdout.write s
   log "\n\n\n\n\n"
-  for y in 0'u8..<self.drawing.size:
+  for y in 0'u8..<size:
     log "          "
-    for x in 0'u8..<self.drawing.size:
+    for x in 0'u8..<size:
       log(
         if self.drawing[x, y]: "██"
         else: "  "
@@ -29,13 +31,21 @@ const svgHeader: string =
   # SVG tag start
   """<svg class="{class}" id="{id}" version="1.1"""" &
   """ xmlns="http://www.w3.org/2000/svg"""" &
-  """ viewBox="-5 -5 {tSize} {tSize}">""" &
+  """ viewBox="-5 -5 {size+10} {size+10}">""" &
   # Background of the SVG
   """<path class="qrLight" fill="{light}"""" &
-  """ d="M-5,-5h{tSize}v{tSize}h-{tSize}Z"></path>"""
+  """ d="M-5,-5h{size+10}v{size+10}h-{size+10}Z"></path>"""
+
+const svgEnd: string = "</svg>"
 
 const modulePath: string =
   """M{x},{y}h1v1h-1Z"""
+
+const modulePathStart: string =
+  """<path class="qrDark" fill="{dark}" d=""""
+
+const modulePathEnd: string =
+  "\"></path>"
 
 const moduleRect: string =
   """<rect class="qrDark qrRounded qrModule"""" &
@@ -46,6 +56,35 @@ const alignmentPatternRect: string =
   """<rect class="qr{m} qrRounded qrAlignment"""" &
   """ fill="{c}" x="{x}" y="{y}" width="{w}" height="{h}"""" &
   """ rx="{r:<.3}"></rect>"""
+
+template drawRegion(ax, bx, ay, by: uint8, s: string) {.dirty.} =
+  for y in ay..<by:
+    for x in ax..<bx:
+      if self.drawing[x, y]:
+        result.add fmt(s)
+
+template drawRegionWithoutAlPatterns(s: string) {.dirty.} =
+  drawRegion 0'u8, size, 7'u8, size-7, s
+  drawRegion 7'u8, size-7, 0'u8, 7'u8, s
+  drawRegion 7'u8, size, size-7, size, s
+
+proc roundedRect(x, y, w, h: uint8, r: float, m, c: string): string =
+  fmt(alignmentPatternRect)
+
+template checkRadius(lvl: range[1'i8..2'i8]): float32 =
+  if alRad == 0: 0f
+  elif alRad-lvl <= 0: 1f / (lvl * 2)
+  else: alRad-lvl
+
+template drawRoundedAlignmentPattern(x, y: uint8): string =
+  roundedRect(x, y, 7'u8, 7'u8, alRad, "dark", dark) &
+  roundedRect(x+1, y+1, 5'u8, 5'u8, checkRadius 1, "light", light) &
+  roundedRect(x+2, y+2, 3'u8, 3'u8, checkRadius 2, "dark", dark)
+
+template drawRoundedAlignmentPatterns {.dirty.} =
+  result.add drawRoundedAlignmentPattern(0'u8, 0'u8)
+  result.add drawRoundedAlignmentPattern(size-7, 0'u8)
+  result.add drawRoundedAlignmentPattern(0'u8, size-7)
 
 proc printSvg*(
   self: DrawedQRCode,
@@ -65,15 +104,11 @@ proc printSvg*(
   ##    changed by setting the `class` parameter, and the `id` can also be set.
   ##    This makes it possible to change properties of the SVG via css.
   ##    The colors can also be changed with the classes `qrLight` and `qrDark`.
-  template tSize: uint8 = self.drawing.size + 10
   result = fmt(svgHeader)
-  # Path drawing the dark modules
-  result.add fmt"""<path class="qrDark" fill="{dark}" d=""""
-  for y in 0'u8..<self.drawing.size:
-    for x in 0'u8..<self.drawing.size:
-      if self.drawing[x, y]: result.add fmt(modulePath)
-  # Close the dark modules path and svg tag
-  result.add "\"></path></svg>"
+  result.add fmt(modulePathStart)
+  drawRegion 0'u8, size, 0'u8, size, modulePath
+  result.add fmt(modulePathEnd)
+  result.add svgEnd
 
 proc printSvg*(
   self: DrawedQRCode,
@@ -87,33 +122,12 @@ proc printSvg*(
   ## but with rounded alignment patterns determined by `alRad` which
   ## can be from `0` (a square) up to `3.5`, which would make it a perfect
   ## circle.
-  template tSize: uint8 = self.drawing.size + 10
   result = fmt(svgHeader)
-  # Path drawing the dark modules
-  result.add fmt"""<path class="qrDark" fill="{dark}" d=""""
-  template drawRegion(ax, bx, ay, by: uint8) {.dirty.} =
-    for y in ay..<by:
-      for x in ax..<bx:
-        if self.drawing[x, y]:
-          result.add fmt(modulePath)
-  drawRegion 0'u8, self.drawing.size, 7'u8, self.drawing.size-7
-  drawRegion 7'u8, self.drawing.size-7, 0'u8, 7'u8
-  drawRegion 7'u8, self.drawing.size, self.drawing.size-7, self.drawing.size
-  result.add "\"></path>"
-  proc roundedRect(x, y, w, h: uint8, r: float, m, c: string): string =
-    fmt(alignmentPatternRect)
-  template checkRadius(lvl: range[1'i8..2'i8]): float =
-    if alRad == 0: 0f
-    elif alRad-lvl <= 0: 1f / (lvl * 2)
-    else: alRad-lvl
-  template roundedAlignmentPattern(x, y: uint8): string =
-    roundedRect(x, y, 7'u8, 7'u8, alRad, "dark", dark) &
-    roundedRect(x+1, y+1, 5'u8, 5'u8, checkRadius 1, "light", light) &
-    roundedRect(x+2, y+2, 3'u8, 3'u8, checkRadius 2, "dark", dark)
-  result.add roundedAlignmentPattern(0'u8, 0'u8)
-  result.add roundedAlignmentPattern(self.drawing.size-7, 0'u8)
-  result.add roundedAlignmentPattern(0'u8, self.drawing.size-7)
-  result.add "</svg>"
+  result.add fmt(modulePathStart)
+  drawRegionWithoutAlPatterns modulePath
+  result.add fmt(modulePathEnd)
+  drawRoundedAlignmentPatterns
+  result.add svgEnd
 
 proc printSvg*(
   self: DrawedQRCode,
@@ -127,28 +141,7 @@ proc printSvg*(
   ## Same as `print<#printSvg%2CDrawedQRCode%2Cstring%2Cstring%2Crange[]>`_
   ## but with with rounded modules determined by `moRad` which can be
   ## from `0` (a square) up to `0.4`, which would make it a perfect circle.
-  template tSize: uint8 = self.drawing.size + 10
   result = fmt(svgHeader)
-  # Path drawing the dark modules
-  template drawRegion(ax, bx, ay, by: uint8) {.dirty.} =
-    for y in ay..<by:
-      for x in ax..<bx:
-        if self.drawing[x, y]:
-          result.add fmt(moduleRect)
-  drawRegion 0'u8, self.drawing.size, 7'u8, self.drawing.size-7
-  drawRegion 7'u8, self.drawing.size-7, 0'u8, 7'u8
-  drawRegion 7'u8, self.drawing.size, self.drawing.size-7, self.drawing.size
-  proc roundedRect(x, y, w, h: uint8, r: float, m, c: string): string =
-    fmt(alignmentPatternRect)
-  template checkRadius(lvl: range[1'i8..2'i8]): float =
-    if alRad == 0: 0f
-    elif alRad-lvl <= 0: 1f / (lvl * 2)
-    else: alRad-lvl
-  template roundedAlignmentPattern(x, y: uint8): string =
-    roundedRect(x, y, 7'u8, 7'u8, alRad, "dark", dark) &
-    roundedRect(x+1, y+1, 5'u8, 5'u8, checkRadius 1, "light", light) &
-    roundedRect(x+2, y+2, 3'u8, 3'u8, checkRadius 2, "dark", dark)
-  result.add roundedAlignmentPattern(0'u8, 0'u8)
-  result.add roundedAlignmentPattern(self.drawing.size-7, 0'u8)
-  result.add roundedAlignmentPattern(0'u8, self.drawing.size-7)
-  result.add "</svg>"
+  drawRegionWithoutAlPatterns moduleRect
+  drawRoundedAlignmentPatterns
+  result.add svgEnd
