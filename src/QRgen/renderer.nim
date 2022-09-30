f45d0f67a106e3681f2a915e760bb8d9bf5866cd
aruZeta <aru_hackZ.official@zohomail.eu>
2022-09-30 08:32:42 +0200

    [add] Module separation forcing in `renderImg`

--- src/QRgen/renderer.nim
+++ src/QRgen/renderer.nim
@@ -46,6 +46,7 @@ proc renderImg*(
   alRad: Percentage = 0,
   moRad: Percentage = 0,
   moSep: Percentage = 25,
+  forceSep: bool = false,
   pixels: uint32 = 512,
   img: Image = Image(width: 0, height: 0),
   imgCoords: tuple[x, y, w, h: uint8] = self.genDefaultCoords
@@ -102,7 +103,7 @@ proc renderImg*(
     drawRegion 0'u8, size, 7'u8, size-7, f
     drawRegion 7'u8, size-7, 0'u8, 7'u8, f
     drawRegion 7'u8, size, size-7, size, f
-  if moRad > 0:
+  if moRad > 0 or forceSep:
     let
       moSepPx: float32 = modulePixels.float32 * 0.4 * moSep / 100
       s: Vec2 = vec2(
@@ -118,11 +119,11 @@ proc renderImg*(
         modulePixels.float32,
         modulePixels.float32
       )
-    if alRad > 0:
+    if alRad > 0 or forceSep:
       drawQRModulesOnly ctx.fillRect(rect(pos, s))
     else:
       drawRegion 0'u8, size, 0'u8, size, ctx.fillRect(rect(pos, s))
-  if alRad > 0 or moRad > 0:
+  if alRad > 0 or moRad > 0 or forceSep:
     let alRadPx: float32 = 3.5 * alRad / 100
     template innerRadius(lvl: static range[0'i8..2'i8]): float32 =
       when lvl == 0: alRadPx
